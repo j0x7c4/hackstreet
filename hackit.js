7bb90956165aa41525b51e1460c744d3ecf7760a
Jie <china.eli@gmail.com>
2013-12-20 20:40:29 +0800

    add street view

--- hackit.js
+++ hackit.js
@@ -1,26 +1,36 @@
+function tg(a) {
+    a = Math.log(Math.tan(.008726646259971648 * (90 + a))) / .017453292519943295;
+    return 111319.49077777778 * a;
+}
+function buildUrl(lat,lng) {
+    x = 111319.49077777778 * lng;
+    y = tg(lat);
+    return "http://sv.map.qq.com/xf?x="+x+"&y="+y+"&r=100";
+}
 $(".map").empty();
-$(".map").append("<div id=pano></div>");
-$("#pano").append("hackit");
+$(".map").append("<div id=pano style='height:236px;width:236px;'></div>");
 //$("#pano").load("http://localhost/~jie/streetview/");
     
 window.qq = window.qq || {};qq.maps = qq.maps || {};window.soso || (window.soso = qq);soso.maps || (soso.maps = qq.maps);
 var loadScriptTime = (new Date).getTime();
 qq.maps.__load = function (apiLoad) {
-        delete qq.maps.__load;
-        apiLoad(["http://open.map.qq.com/", "v2.0.10", "", 1387523864228, true, "d84d6d83e0e51e481e50454ccbe8986b", "20130701", "apifiles/v2/0/10/mods/", "apifiles/v2/0/10/theme/"],loadScriptTime);
-    };
-    
+    delete qq.maps.__load;
+    apiLoad(["http://open.map.qq.com/", "v2.0.10", "", 1387523864228, true, "d84d6d83e0e51e481e50454ccbe8986b", "20130701", "apifiles/v2/0/10/mods/", "apifiles/v2/0/10/theme/"],loadScriptTime);
+};
+//$(".shop-location").find('li')
+lat = 31.232685;
+lng = 121.471046;
+posUrl = buildUrl(lat,lng);
+$.get( posUrl, function( data ) {
     var pano = new window.qq.maps.Panorama(document.getElementById('pano'), {
-        pano: '10051001111220105028000',
-        disableMove: false,
-        pov:{
-            heading:20,
-            pitch:0
-        },
-        zoom:3
-    });
+    pano: '10021010130505112127600',
+    disableMove: false,
+    pov:{
+        heading:20,
+        pitch:0
+    },
+    zoom:3
+});
+});
 
-    pano_service = new qq.maps.PanoramaService();
-    pano_service.getPano(new qq.maps.LatLng(31.21533,121.42031), 100, function (result){
-                pano.setPano(result.svid);
-            });
+
