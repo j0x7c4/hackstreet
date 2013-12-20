d7d9e4662fe8b2c0d836d16e8be324117e9082ee
shenyitao <397901611@qq.com>
2013-12-21 07:59:47 +0800

    popup

--- hackit.js
+++ hackit.js
@@ -11,10 +11,33 @@ function buildGoogleUrl(address) {
     return "http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=true";
 }
 
+var UNIQUE_MAP_VIEWER_ID = 'crx_myextension_iframe'; 
+var latitude = -1;
+var longitude = -1;
+
+/**
+ * Here is where you want to render a latitude and longitude. We create an iframe so we
+ * we can inject it. We just want to maintain a single instance of it though.
+ */
+function onRenderMap() {
+  var mapViewerDOM = document.getElementById(UNIQUE_MAP_VIEWER_ID);
+  if (mapViewerDOM) {
+     mapViewerDOM.parentNode.removeChild(mapViewerDOM);
+  }
+
+  mapViewerDOM = document.createElement('iframe');
+
+  document.body.appendChild(mapViewerDOM);
+};
+
+$("body").append('<div id="modal-panel" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="modal-label">提示</h3></div><div id="map-canvas" style="width:862px;height:500px;"></div></div>');
 $(".map").empty();
 $(".map").append("<div id=pano style='height:236px;width:236px;'></div>");
+$(".map-info").append("<a id=btn_map href=#>  Hack地图</a>");
+
+
 //$("#pano").load("http://localhost/~jie/streetview/");
-    
+googleMapApiUrl = "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false" 
 window.qq = window.qq || {};qq.maps = qq.maps || {};window.soso || (window.soso = qq);soso.maps || (soso.maps = qq.maps);
 var loadScriptTime = (new Date).getTime();
 qq.maps.__load = function (apiLoad) {
@@ -28,6 +51,22 @@ googleUrl = buildGoogleUrl(address);
 $.getJSON( googleUrl , function (data) {
     lat = data.results[0].geometry.location.lat;
     lng = data.results[0].geometry.location.lng;
+    $("#btn_map").click((function(lat,lng){
+        return function ( ) {
+            $.getScript(googleMapApiUrl,function(data,status){
+                addStoreFromLocalStorage();
+                $('#clear').click(function(){
+                    $('#modal-panel').modal({backdrop: 'static', keyboard: false});
+                    initializeMap({lat: lat, lng: lng});
+                    var shopList = storeSet.keys();
+                    showStoreByList(shopList);
+                    //google.maps.event.addDomListener(window, 'load', initialize);
+            });
+          });
+        
+        };
+    })(lat,lng)
+    );
     posUrl = buildUrl(lat,lng);
     $.get( posUrl, function( data ) {
         pid = data.match("\"svid\":\"([0-9]+)")[1];
@@ -35,10 +74,10 @@ $.getJSON( googleUrl , function (data) {
             pano: pid,
             disableMove: false,
             pov:{
-                heading:20,
+                heading:100,
                 pitch:0
             },
-            zoom:3
+            zoom:1
         });
     },"text");
 });
