5269bca1f0abbbc8d02ad3341e4efd87a1a7de77
Jie <china.eli@gmail.com>
2013-12-21 03:26:20 +0800

    merge1

--- hackit.js
+++ hackit.js
@@ -10,12 +10,14 @@ function buildUrl(lat,lng) {
 function buildGoogleUrl(address) {
     return "http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=true";
 }
-
+$("body").append('<div id="modal-panel" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="modal-label">提示</h3></div><div id="map-canvas" style="width:862px;height:500px;"></div></div>');
 $(".map").empty();
 $(".map").append("<div id=pano style='height:236px;width:236px;'></div>");
-$(".map-info").append("<a href=#>  Hack地图</a>");
+$(".map-info").append("<a id=btn_map href=#>  Hack地图</a>");
+
+
 //$("#pano").load("http://localhost/~jie/streetview/");
-    
+googleMapApiUrl = "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false" 
 window.qq = window.qq || {};qq.maps = qq.maps || {};window.soso || (window.soso = qq);soso.maps || (soso.maps = qq.maps);
 var loadScriptTime = (new Date).getTime();
 qq.maps.__load = function (apiLoad) {
@@ -29,6 +31,22 @@ googleUrl = buildGoogleUrl(address);
 $.getJSON( googleUrl , function (data) {
     lat = data.results[0].geometry.location.lat;
     lng = data.results[0].geometry.location.lng;
+    $("#btn_map").click((function(lat,lng){
+        return function ( ) {
+            $.getScript(googleMapApiUrl,function(data,status){
+                addStoreFromLocalStorage();
+                $('#clear').click(function(){
+                    $('#modal-panel').modal({backdrop: 'static', keyboard: false});
+                    initializeMap({lat: lat, lng: lng});
+                    var shopList = storeSet.keys();
+                    showStoreByList(shopList);
+                    //google.maps.event.addDomListener(window, 'load', initialize);
+            });
+          });
+        
+        };
+    })(lat,lng)
+    );
     posUrl = buildUrl(lat,lng);
     $.get( posUrl, function( data ) {
         pid = data.match("\"svid\":\"([0-9]+)")[1];
